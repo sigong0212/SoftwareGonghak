12de8ccfb0bd41115cdf826e6b581f674fdb2602
sigong0212 <yuj0317@naver.com>
2023-12-02 13:01:13 +0900

    문장 추가 중

--- 1127/script.js
+++ 1127/script.js
@@ -4,7 +4,10 @@ function processForm() {
 
   if (userInput.endsWith("이")) {
       responseStr = "이비인후과에 가면 귀, 코, 목 등의 진료를 받을 수 있습니다.";
-  } else {
+   } else if (userInput.endsWith("지")) {
+    responseStr = "지렁이는 비 오는 날 자주 볼 수 있어요";
+  } 
+  else {
       responseStr = "안녕하세요.";
   }
 
